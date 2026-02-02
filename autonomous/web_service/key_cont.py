55468c2a8824aa2a659e4659ba4876ac0fb07e82
DESKTOP-HE2FDBA\seung <osy044@naver.com>
2026-02-02 16:06:15 +0900

    키보드 업데이트

--- autonomous/web_service/key_cont.py
+++ autonomous/web_service/key_cont.py
@@ -3,9 +3,11 @@ import motor_cont
 key = None
 
 def handle_key(keys):
+    # 소문자로 변환하여 대소문자 구분 없이 처리
+    keys = [k.lower() for k in keys]
 
     # 모든 키가 눌리거나, 아무 키도 눌리지 않거나, 3개의 키가 눌리거나, ↑↓ 또는 ←→가 눌리면
-    if (len(keys) == 3) or (len(keys) == 4) or ('ArrowUp' in keys and 'ArrowDown' in keys) or ('ArrowLeft' in keys and 'ArrowRight' in keys):
+    if (len(keys) == 3) or (len(keys) == 4) or ('arrowup' in keys and 'arrowdown' in keys) or ('arrowleft' in keys and 'arrowright' in keys):
         motor_cont.drive(go_flag=0, left_flag=0, right_flag=0, brake_flag=1, back_flag=0)
 
     # 아무 키도 눌리지 않을 때
@@ -14,11 +16,11 @@ def handle_key(keys):
         motor_cont.drive(go_flag=0, left_flag=0, right_flag=0, brake_flag=0, back_flag=0)
 
     # ↑ 키가 눌렸을 때
-    elif 'ArrowUp' in keys:
-        if 'ArrowLeft' in keys:
+    elif 'arrowup' in keys:
+        if 'arrowleft' in keys:
             print("Left Turn")
             motor_cont.drive(go_flag=1, left_flag=1, right_flag=0, brake_flag=0, back_flag=0)
-        elif 'ArrowRight' in keys:
+        elif 'arrowright' in keys:
             print("Right Turn")
             motor_cont.drive(go_flag=1, left_flag=0, right_flag=1, brake_flag=0, back_flag=0)
         else:
@@ -26,11 +28,11 @@ def handle_key(keys):
             motor_cont.drive(go_flag=1, left_flag=0, right_flag=0, brake_flag=0, back_flag=0)
 
     # ↓ 키가 눌렸을 때
-    elif 'ArrowDown' in keys:
-        if 'ArrowLeft' in keys:
+    elif 'arrowdown' in keys:
+        if 'arrowleft' in keys:
             print("Left Back")
             motor_cont.drive(go_flag=0, left_flag=1, right_flag=0, brake_flag=0, back_flag=1)
-        elif 'ArrowRight' in keys:
+        elif 'arrowright' in keys:
             print("Right Back")
             motor_cont.drive(go_flag=0, left_flag=0, right_flag=1, brake_flag=0, back_flag=1)
         else:
@@ -38,12 +40,12 @@ def handle_key(keys):
             motor_cont.drive(go_flag=0, left_flag=0, right_flag=0, brake_flag=0, back_flag=1)
 
     # ← 키가 눌렸을 때
-    elif 'ArrowLeft' in keys:
+    elif 'arrowleft' in keys:
         print("Left")
         motor_cont.drive(go_flag=0, left_flag=1, right_flag=0, brake_flag=0, back_flag=0)
 
     # → 키가 눌렸을 때
-    elif 'ArrowRight' in keys:
+    elif 'arrowright' in keys:
         print("Right")
         motor_cont.drive(go_flag=0, left_flag=0, right_flag=1, brake_flag=0, back_flag=0)
         
